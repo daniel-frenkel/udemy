48a5b743761f8a4d0e9ba89e8871849f072a1a46
Daniel Frenkel <daniel@valarsystems.com>
2023-05-25 10:40:46 -0700

    Update Firmware.ino

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -19,53 +19,26 @@ void setup() {
   setup_motors();
   API();
   ESPUIsetup();
-
-  // Now set up tasks to run independently.
-  xTaskCreatePinnedToCore(
-    MotorTask //Motor Task
-    ,  "MotorTask"   // A name just for humans
-    ,  1024 * 4 // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  3  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL
-    ,  1);
 }
 
 void loop()
 {
-  // Empty. Things are done in Tasks.
-}
-
-/*--------------------------------------------------*/
-/*---------------------- Tasks ---------------------*/
-/*--------------------------------------------------*/
-
-void MotorTask(void *pvParameters)
-{
-  (void) pvParameters;
-  for (;;)
+  if (run_motor == true)
   {
-    if (run_motor == true)
-    {
-      Serial.println("Run Motor Function");
-      move_motor();
-      run_motor = false;
-      ESPUI.updateLabel(positionLabel, String(int(((float)current_position / (float)max_steps) * 100)));
-      Serial.println("Motor Complete");
-    }
-    else if (set_zero == true)
-    {
-      setZero();
-      set_zero = false;
-    }
-    else if (wifi_button == true)
-    {
-      button_change();
-      wifi_button = false;
-    }
-    else
-    {
-      vTaskDelay(1);
-    }
+    Serial.println("Run Motor Function");
+    move_motor();
+    run_motor = false;
+    ESPUI.updateLabel(positionLabel, String(int(((float)current_position / (float)max_steps) * 100)));
+    Serial.println("Motor Complete");
+  }
+  else if (set_zero == true)
+  {
+    setZero();
+    set_zero = false;
+  }
+  else if (wifi_button == true)
+  {
+    button_change();
+    wifi_button = false;
   }
 }
