b1ee1eab74c6aceb48adfe27ca021e5fd20f81f3
Daniel Frenkel <daniel@valarsystems.com>
2023-05-25 12:52:28 -0700

    Updates

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,24 +1,17 @@
 #include <Arduino.h>
-#include <ESPUI.h>
 #include "FastAccelStepper.h"
 #include <HardwareSerial.h>
 #include <TMCStepper.h>
 #include <SPI.h>
-#include <Preferences.h>
-#include "Memory.h"
-#include "ResetButton.h"
 #include "MotorControl.h"
-#include "API.h"
-#include "ESPUI.h"
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  preferences.begin("local", false);
-  load_preferences();
   setup_motors();
-  API();
-  ESPUIsetup();
+
+  //Change this value to set the position (Negative will reverse)
+  move_to_step = 5000;
+  run_motor = true;
 }
 
 void loop()
@@ -28,17 +21,6 @@ void loop()
     Serial.println("Run Motor Function");
     move_motor();
     run_motor = false;
-    ESPUI.updateLabel(positionLabel, String(int(((float)current_position / (float)max_steps) * 100)));
     Serial.println("Motor Complete");
-  }
-  else if (set_zero == true)
-  {
-    setZero();
-    set_zero = false;
-  }
-  else if (wifi_button == true)
-  {
-    button_change();
-    wifi_button = false;
   }
 }
